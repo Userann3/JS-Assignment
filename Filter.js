85f0e85ea4fab13245c828f7a7204cbb737f334a
Username3 <aadityarana900@gmail.com>
2024-07-28 14:56:42 +0530

    Js Assignments

---
+++ Filter.js
@@ -0,0 +1,119 @@
+// Question 1: Filter Even Numbers
+// Given a list of integers, use the `filter` function to create a new list that contains only the even numbers.
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+
+// function even(n){
+//     return n%2==0;
+// }
+
+// let res=arr.filter(even)
+// console.log(res)
+
+// Question 2: Filter Strings with Length Greater Than 3
+// Given a list of strings, use the `filter` function to create a new list that contains only the strings with length greater than 3.
+
+// let list=['ram','shyam','pinku','minku','adi','anu']
+
+// function check(n){
+//     return n.length==3
+// }
+
+// let res=list.filter(check)
+// console.log(res)
+
+
+// Question 3: Filter Positive Numbers
+// Given a list of integers, use the `filter` function to create a new list that contains only the positive numbers.
+
+// let arr=[25,-5,88,-2,-10,+45,0]
+
+// function check(n){
+//     return n>=0;
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
+// Question 4: Filter Strings Containing 'a'
+// Given a list of strings, use the `filter` function to create a new list that contains only the strings that have the letter 'a' in them.
+
+// let arr=['aman','naman','amit','ankit','tanu','mannu','chintu']
+
+// function check(n){
+//     return n[0]=='a'
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
+// Question 5: Filter Numbers Greater Than 10
+// Given a list of integers, use the `filter` function to create a new list that contains only the numbers greater than 10.
+
+// let arr=[10,456,2,25,11,5,0]
+
+// function check(n){
+//     return n>10;
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
+// Question 6: Filter Non-Empty Strings
+// Given a list of strings, use the `filter` function to create a new list that contains only the non-empty strings.
+
+// let arr=['res','','green','yellow','0','','']
+
+// function check(n){
+//     return n!=''
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
+// Question 7: Filter Odd Numbers
+// Given a list of integers, use the `filter` function to create a new list that contains only the odd numbers.
+
+// let arr=[1,2,3,4,5,6,7,8,9]
+
+// function even(n){
+//     return n%2!=0;
+// }
+
+// let res=arr.filter(even)
+// console.log(res)
+
+// Question 8: Filter Strings Starting with 'A'
+// Given a list of strings, use the `filter` function to create a new list that contains only the strings that start with the letter 'A'.
+
+// let arr=['aman','naman','amit','Ankit','tanu','mannu','chintu','Anuj']
+// function check(n){
+//     return n[0]=='A'
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
+// Question 9: Filter Multiples of 5
+// Given a list of integers, use the `filter` function to create a new list that contains only the multiples of 5.
+
+// let arr=[1,2,3,55,5,6,20,8,15]
+
+// function check(n){
+//     return n%5==0;
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
+// Question 10: Filter Strings with Uppercase Letters
+// Given a list of strings, use the `filter` function to create a new list that contains only the strings that have uppercase letters.
+
+// let arr=['aman','NAMAN','amit','Ankit','Tanu','mannu','chintu','Anuj']
+// function check(n){
+//     return n.match(/[A-Z]/g)
+// }
+
+// let res=arr.filter(check)
+// console.log(res)
+
---
+++ functions.js
@@ -0,0 +1,22 @@
+// Problem1:
+// Write a function to count the total odd and total even numbers by taking the start point and end point as arguments.
+
+// function check(start,end){
+//     for (start ; start<=end ; start++){
+//         let num = start%2==0 ? `${start}:Even` : `${start}:Odd`
+//         console.log(num)
+//         // if(start%2==0){
+//         //     console.log("Even numbers are : ",start);
+//         // }
+//         // else{
+//         //     console.log("Odd numbers are : ",start);
+//         //     }
+//         }
+// }
+
+// check(1,5)
+
+// Problem2:
+// Write a function to filter out the greatest string from a given array `['python', 'javascript', 'arguments', 'java']`.
+
+
